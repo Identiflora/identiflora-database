b49b5875b7a097c5a600bea1e75dc95ba1dae84f
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-12-03 14:40:33 -0800

    testing for inserting plant species

--- testing/plant_species_test.sql
+++ testing/plant_species_test.sql
@@ -1,6 +1,6 @@
 INSERT INTO plant_species (common_name, scientific_name, img_url)
     VALUES (
-            'test_name',
-            'test_sci_name',
-            'API_test_img.png'
+            'test_common_name',
+            'Tradescantia pallida (Rose) D.R.Hunt',
+            'https://t4.ftcdn.net/jpg/05/29/62/89/360_F_529628989_a6x0Ho7ENtGcLd0lGQQbewYK41NBbOhO.jpg'
             );
